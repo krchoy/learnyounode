7c9f2f069355d66ffa86cb308f359dc4a1f4327a
krchoy <krchoy@gmail.com>
2015-03-25 09:57:17 +1100

    step 4

--- program4.js
+++ program4.js
@@ -4,18 +4,21 @@ var myStringArray;
 var myString;
 
 function processTheFile(cb) {
-  console.log('1 ' + myString);
+//  console.log('1 ' + myString);
   fs.readFile(process.argv[2], function doneReading(err, myBuff){
     myString = myBuff.toString();
-    console.log('2 ' + myString);
+//   console.log('2 ' + myString);
     myStringArray = myString.split('\n');
+//   console.log('3 ' + myStringArray);
+    cb();
   });
 
-  cb(myStringArray)
+
 }
-function writeItOut(myStringArray) {
-  console.log ('3 ' + myString);
+function writeItOut() {
+// console.log ('4 ' + myStringArray);
+  console.log (myStringArray.length- 1);
 }
 
-console.log ('0 ');
+// console.log ('0 ');
 processTheFile(writeItOut);
